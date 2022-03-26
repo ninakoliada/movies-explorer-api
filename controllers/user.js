021eeb0985bbdb77bfeb56d8cc9645cea39077e1
pinakoladda <pinakoladda@ya.ru>
2022-03-26 14:38:30 +0300

    password hash

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcrypt');
 const AuthError = require('../errors/auth-error');
+const DuplicateError = require('../errors/duplicate-error');
+const NotFoundError = require('../errors/not-found-error');
 const Users = require('../models/user');
 
 const getUsers = async (req, res, next) => {
@@ -11,36 +14,45 @@ const getUsers = async (req, res, next) => {
   }
 };
 
-// const updateUser = async (req, res, next) => {
-//   try {
-//     const { name, email } = req.body;
+const updateUser = async (req, res, next) => {
+  try {
+    const { name, email } = req.body;
 
-//     const user = await Users.findByIdAndUpdate(
-//       req.user._id,
-//       { name, email },
-//       { new: true, runValidators: true },
-//     );
+    const user = await Users.findByIdAndUpdate(
+      req.user._id,
+      { name, email },
+      { new: true, runValidators: true },
+    );
 
-//     // if (!user) {
-//     //   return next(new NotFoundError('Запрашиваемый пользователь не найден'));
-//     // }
+    if (!user) {
+      return next(new NotFoundError('Запрашиваемый пользователь не найден'));
+    }
 
-//     return res.send(user);
-//   } catch (error) {
-//     return next(error);
-//   }
-// };
+    return res.send(user);
+  } catch (error) {
+    return next(error);
+  }
+};
 
 const createUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
     const user = await Users.create({
       name,
       email,
-      password,
+      password: passwordHash,
     });
+
+    user.password = undefined;
     return res.send(user);
   } catch (error) {
+    if (error.code === 11000) {
+      return next(new DuplicateError('Пользователь с таким email уже зарегистрирован'));
+    }
+
     return next(error);
   }
 };
@@ -48,9 +60,15 @@ const createUser = async (req, res, next) => {
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const user = await Users.findOne({ email, password });
+    const user = await Users.findOne({ email }).select('+password');
 
     if (!user) {
+      return next(new AuthError('Неправильные почта или пароль'));
+    }
+
+    const matched = await bcrypt.compare(password, user.password);
+
+    if (!matched) {
       return next(new AuthError('Неправильные почта или пароль'));
     }
 
@@ -60,4 +78,9 @@ const login = async (req, res, next) => {
   }
 };
 
-module.exports = { getUsers, createUser, login };
+module.exports = {
+  getUsers,
+  createUser,
+  updateUser,
+  login,
+};
