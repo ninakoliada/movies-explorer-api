31d338c7ea9f42860183d0a250818d2a48dc44c5
pinakoladda <pinakoladda@ya.ru>
2022-04-28 17:40:13 +0300

    Increese limit of request

--- middlewares/limitter.js
+++ middlewares/limitter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 module.exports = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 120 * 60 * 1000,
   max: 100,
   standardHeaders: true,
   legacyHeaders: false,
